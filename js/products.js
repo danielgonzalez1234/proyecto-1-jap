c684eef050b4b24d886640d3f56a341c555a8a1c
bigjhonny <djgt.uy@icloud.com>
2020-08-16 23:33:47 -0300

    entrega 1
    entrega limite para la entrega 1

--- js/products.js
+++ js/products.js
@@ -4,3 +4,40 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+var productsArray = [];
+
+function showProductsList(array) {
+    var htmlcontenidos = "";
+    for (let aux = 0; aux < array.length; aux++) {
+        //moverse por el array
+        var product = array[aux];
+        //obtencion de los datos desglosados.
+        htmlcontenidos += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name + `</h4>
+                        <small class="font-muted">` + product.currency + " $" + product.cost + `<br> `+ product.soldCount + ` artículos vendidos </small>                    
+                     </div>
+                     ${product.description}
+                 </div>
+             </div>
+         </div>
+        `
+        // devolucion de los datos return
+        document.getElementById("prod-list-container").innerHTML = htmlcontenidos;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
+});
---
+++ js/redirecciones.js
@@ -0,0 +1,12 @@
+//Función que redirecciona a un lado. 
+
+function direccion(numeros){
+    //8 inicio
+    //2 login
+    if (numeros==1){
+        document.location.href = 'login.html';
+      }
+    if (numeros==8){
+        document.location.href= 'index.html';
+    }
+}
